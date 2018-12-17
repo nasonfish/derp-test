7e89140e43e1adba03c37a3e3a50389685d33954
Daniel Barnes <daniel@danielbarnes.me>
2018-12-16 17:31:56 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 # do the execution/evaluation
 
+successes = []
+
+for i in range(1,0xFFFFFFFF):
+    success = bitSplit(i) == 1235354
+    if success:
+      successes.append(i)
+
+if len(successes) == 0xFFFFFFFF:
+  return 100
+else:
+  return(successes/0xFFFFFF, "You succeeded the following test cases", successes)
